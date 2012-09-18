3cbf07a487362c05b8a8811d262f8c66cf08ee9f
Mason Adkins <zappalives0@gmail.com>
2012-09-18 11:25:46 -0400

    Added plugin/yml

--- src/me/Jeebiss/Assassin/Assassin.java
+++ src/me/Jeebiss/Assassin/Assassin.java
@@ -1,22 +1,28 @@
 package me.Jeebiss.Assassin;
 
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.trait.TraitInfo;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
-//This is your bukkit plugin class. Use it to hook your trait into Citizens and handle any commands.
-public class Assassin extends org.bukkit.plugin.java.JavaPlugin {
+public class Assassin extends JavaPlugin {
 
 	public void onEnable() {
-		//check if Citizens is present and enabled.
-		if(getServer().getPluginManager().getPlugin("Citizens") == null || getServer().getPluginManager().getPlugin("Citizens").isEnabled() == false) {
-			//Register your trait with Citizens.        
-			net.citizensnpcs.api.trait.TraitInfo info = net.citizensnpcs.api.trait.TraitInfo.create(AssassinTrait.class).withName("Assassin");
-			net.citizensnpcs.api.CitizensAPI.getTraitFactory().registerTrait(info);	
+		if(getServer().getPluginManager().getPlugin("Citizens") != null) {
+			if(getServer().getPluginManager().getPlugin("Citizens").isEnabled()) { 
+				CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(AssassinTrait.class).withName("assassin"));
+			}
 		}
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] inargs) {
+		if(cmd.getName().equalsIgnoreCase("assassin")){
+			sender.sendMessage("Assassin v0.1 for Citizens 2!");
+			return true;
+		}
+		
 		return false;
 	}
 }
